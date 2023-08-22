ea6edc639b236aac157a2b9b53d962a6cdd9cdc8
Diego Belay <diego.belay@gmail.com>
2023-08-22 16:53:55 -0300

    Unlock cursor on main menu

--- Assets/Scripts/Game/Menu/MainMenu.cs
+++ Assets/Scripts/Game/Menu/MainMenu.cs
@@ -5,6 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Start()
+    {
+        Debug.Log("[MainMenu] Start!");
+
+        UnlockCursor();
+    }
+
+    void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
