14211e73022827bc7492d9b65c9d3c45dcb2fc38
Diego Belay <diego.belay@gmail.com>
2023-09-26 18:00:47 -0300

    Persist audio settings

--- Assets/Scripts/Game/Menu/OptionsMenu.cs
+++ Assets/Scripts/Game/Menu/OptionsMenu.cs
@@ -2,31 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Audio;
 using System;
 
-[RequireComponent(typeof(AudioSource))]
 public class OptionsMenu : MonoBehaviour
 {
-    const float DEFAULT_MUSIC_VOLUME = 0;
-    const float DEFAULT_SFX_VOLUME = 0f;
-
-    [SerializeField] AudioMixer _audioMixer;
-    [SerializeField] AudioClip _sfxSample;
- 
     [Header("Slider range [-80db, 0db]")]
     [SerializeField] Slider _musicVolumeSlider;
     [SerializeField] Slider _sfxVolumeSlider;
-
-    AudioSource _audioSource;
+        
 
     void Start()
     {
-        _audioSource = GetComponent<AudioSource>();
-
-        _musicVolumeSlider.value = LoadMusicSetting();
-
-        _sfxVolumeSlider.value = LoadSFXSetting();
+        _musicVolumeSlider.value = PlayerSettings.Instance.LoadMusicSetting();
+        _sfxVolumeSlider.value = PlayerSettings.Instance.LoadSFXSetting();
     }
 
     #region Slider_Events
@@ -34,12 +22,12 @@ public class OptionsMenu : MonoBehaviour
     public void OnMusicVolumeChange()
     {
         float newVolume = _musicVolumeSlider.value;
-        
+
         // mixer
-        SetAudioMixerMusicVolume(newVolume);
+        PlayerSettings.Instance.SetAudioMixerMusicVolume(newVolume);
 
         // persist
-        SaveMusicSetting(newVolume);
+        PlayerSettings.Instance.SaveMusicSetting(newVolume);
     }
 
     public void OnSFXVolumeChange()
@@ -47,67 +35,17 @@ public class OptionsMenu : MonoBehaviour
         float newVolume = _sfxVolumeSlider.value;
 
         // mixer
-        bool volumeChanged = SetAudioMixerSFXVolume(newVolume);
+        bool volumeChanged = PlayerSettings.Instance.SetAudioMixerSFXVolume(newVolume);
 
         if (volumeChanged == false)
         {
             return;
         }
 
-        // play sample
-        if (!_audioSource.isPlaying)
-        {
-            _audioSource.PlayOneShot(_sfxSample);
-        }
-
         // persist
-        SaveSFXSetting(newVolume);
+        PlayerSettings.Instance.SaveSFXSetting(newVolume);
     }
 
     #endregion
-
-    void SetAudioMixerMusicVolume(float volume)
-    {
-        // Music on Audio Mixer
-        _audioMixer.SetFloat("MusicVolume", volume);
-    }
-
-    bool SetAudioMixerSFXVolume(float volume)
-    {
-        _audioMixer.GetFloat("SFXVolume", out float currentVolume);
-
-        if (currentVolume == volume)
-        {
-            return false;
-        }
-
-        // SFX on Audio Mixer
-        _audioMixer.SetFloat("SFXVolume", volume);
-
-        return true;
-    }
-
-    #region Persistance
-
-    void SaveMusicSetting(float volume)
-    {
-    }
-
-    void SaveSFXSetting(float volume)
-    {    
-    }
-
-    float LoadMusicSetting()
-    {
-        return DEFAULT_MUSIC_VOLUME;
-    }
-
-    float LoadSFXSetting()
-    {
-        return DEFAULT_SFX_VOLUME;
-    }
-
-    #endregion
-
 
 }
---
+++ Assets/Scripts/Yxp/PlayerSettings/PlayerSettings.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayerSettings : MonoBehaviour
+{
+    [SerializeField] AudioMixer _audioMixer;
+    [SerializeField] AudioClip _sfxSample;
+
+    const float DEFAULT_MUSIC_VOLUME = 0;
+    const float DEFAULT_SFX_VOLUME = 0f;
+
+    const string PERSISTANCE_MUSIC_KEY = "music-volume";
+    const string PERSISTANCE_SFX_KEY = "sfx-volume";
+
+    AudioSource _audioSource;
+
+    #region Instance
+
+    private static PlayerSettings _instance;
+
+    public static PlayerSettings Instance { get { return _instance; } }
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    #endregion
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        ApplyPlayerSettingsAudio();
+    }
+
+    void ApplyPlayerSettingsAudio()
+    {
+        SetAudioMixerMusicVolume(LoadMusicSetting());
+        SetAudioMixerSFXVolume(LoadSFXSetting());
+    }
+
+    #region Audio
+
+    public void SetAudioMixerMusicVolume(float volume)
+    {
+        // Music on Audio Mixer
+        _audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public bool SetAudioMixerSFXVolume(float volume)
+    {
+        _audioMixer.GetFloat("SFXVolume", out float currentVolume);
+
+        if (currentVolume == volume)
+        {
+            return false;
+        }
+
+        // SFX on Audio Mixer
+        _audioMixer.SetFloat("SFXVolume", volume);
+
+        if (!_audioSource.isPlaying)
+        {
+            _audioSource.PlayOneShot(_sfxSample);
+        }
+
+        return true;
+    }
+
+    #region Persistance
+
+    public void SaveMusicSetting(float value)
+    {
+        PlayerPrefs.SetFloat(PERSISTANCE_MUSIC_KEY, value);
+    }
+
+    public void SaveSFXSetting(float value)
+    {
+        PlayerPrefs.SetFloat(PERSISTANCE_SFX_KEY, value);
+    }
+
+    public float LoadMusicSetting()
+    {
+        if (PlayerPrefs.HasKey(PERSISTANCE_MUSIC_KEY) == false)
+        {
+            // first run
+            SaveMusicSetting(DEFAULT_MUSIC_VOLUME);
+
+            return DEFAULT_MUSIC_VOLUME;
+        }
+
+        return PlayerPrefs.GetFloat(PERSISTANCE_MUSIC_KEY);
+    }
+
+    public float LoadSFXSetting()
+    {
+        if (PlayerPrefs.HasKey(PERSISTANCE_SFX_KEY) == false)
+        {
+            // first run
+            SaveSFXSetting(DEFAULT_SFX_VOLUME);
+
+            return DEFAULT_SFX_VOLUME;
+        }
+
+        return PlayerPrefs.GetFloat(PERSISTANCE_SFX_KEY);
+    }
+
+    #endregion
+
+    #endregion
+}
